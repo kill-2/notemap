81a7f6ab11a5b77e363d9027057c38ad2ea4e08d
c <0xe0ffffgmail.com>
2025-08-07 15:53:44 +0800

    extract models config

--- analyzer.py
+++ analyzer.py
@@ -4,6 +4,34 @@ from dataclasses import dataclass
 
 from openai import OpenAI
 
+models = [
+    {
+        "env": "NOTEMAP_DEEPSEEK",
+        "url": "https://api.deepseek.com",
+        "mod": "deepseek-chat",
+    },
+    {
+        "env": "NOTEMAP_KIMI_K2",
+        "url": "https://api.moonshot.cn/v1",
+        "mod": "kimi-k2-0711-preview",
+    },
+    {
+        "env": "NOTEMAP_OPENROUTER_DEEPSEEK_CHAT_V3_0324_FREE",
+        "url": "https://openrouter.ai/api/v1",
+        "mod": "deepseek/deepseek-chat-v3-0324:free",
+    },
+    {
+        "env": "NOTEMAP_OPENROUTER_KIMI_K2_FREE",
+        "url": "https://openrouter.ai/api/v1",
+        "mod": "moonshotai/kimi-k2:free",
+    },
+    {
+        "env": "NOTEMAP_OPENROUTER_QWEN3_235B_A22B_FREE",
+        "url": "https://openrouter.ai/api/v1",
+        "mod": "qwen/qwen3-235b-a22b:free",
+    },
+]
+
 prompt = """
 I will give you some code snippets, then you tell me each snippet read data from where, and write data to where, if they do read or write.
 
@@ -67,25 +95,10 @@ def parse(snippets: str) -> list[IO]:
 
 
 def request(snippets: str) -> str:
-    api_key = os.getenv("NOTEMAP_DEEPSEEK") or os.getenv("NOTEMAP_KIMI_K2") or os.getenv("NOTEMAP_OPENROUTER_DEEPSEEK_CHAT_V3_0324_FREE") or os.getenv("NOTEMAP_OPENROUTER_KIMI_K2_FREE") or os.getenv("NOTEMAP_OPENROUTER_QWEN3_235B_A22B_FREE")
-    if os.getenv("NOTEMAP_DEEPSEEK"):
-        base_url = "https://api.deepseek.com"
-        model = "deepseek-chat"
-    elif os.getenv("NOTEMAP_KIMI_K2"):
-        base_url = "https://api.moonshot.cn/v1"
-        model = "kimi-k2-0711-preview"
-    elif os.getenv("NOTEMAP_OPENROUTER_DEEPSEEK_CHAT_V3_0324_FREE"):
-        base_url = "https://openrouter.ai/api/v1"
-        model = "deepseek/deepseek-chat-v3-0324:free"
-    elif os.getenv("NOTEMAP_OPENROUTER_KIMI_K2_FREE"):
-        base_url = "https://openrouter.ai/api/v1"
-        model = "moonshotai/kimi-k2:free"
-    elif os.getenv("NOTEMAP_OPENROUTER_QWEN3_235B_A22B_FREE"):
-        base_url = "https://openrouter.ai/api/v1"
-        model = "qwen/qwen3-235b-a22b:free"
-    else:
-        base_url = "https://api.deepseek.com"
-        model = "deepseek-chat"
+    config = _get_model_config()
+    api_key = os.getenv(config["env"])
+    base_url = config["url"]
+    model = config["mod"]
 
     client = OpenAI(api_key=api_key, base_url=base_url)
     response = client.chat.completions.create(
@@ -94,3 +107,10 @@ def request(snippets: str) -> str:
         response_format={"type": "json_object"},
     )
     return response.choices[0].message.content
+
+
+def _get_model_config() -> dict[str, str]:
+    for model in models:
+        if os.getenv(model["env"]):
+            return model
+    return models[0]
